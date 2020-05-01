90728a4d0ba653d895accbf160f71705bafe5f15
Ibán López Pérez <>
2020-05-01 22:18:48 +0200

    add github repo link to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@
 import { Styled, jsx } from "theme-ui"
 import React from "react"
 
+import Icon from './icons'
+
 const Footer = () => {
   return (
     <footer
@@ -17,9 +19,11 @@ const Footer = () => {
           maxWidth: 960,
           py: 4,
           px: 3,
+          display: `flex`,
+          justifyContent: `space-between`
         }}
       >
-        <Styled.p sx={{ color: `background` }}>
+        <Styled.p sx={{ color: `background`, m: 0 }}>
           © {new Date().getFullYear()}, Built with
           {` `}
           <Styled.a
@@ -29,6 +33,12 @@ const Footer = () => {
             Gatsby
           </Styled.a>
         </Styled.p>
+        <Styled.a
+            sx={{ color: `background` }}
+            href="https://github.com/ibanlopez/gatsby-starter-base-theme-ui"
+          >
+            <Icon IconName="github" />
+          </Styled.a>
       </div>
     </footer>
   )
